d33df9e658f7a3fb9912a378d3d3da0d72830ae5
Phinkurniawan <2072035@maranatha.ac.id>
2023-03-15 15:26:07 +0700

    table up page

---
+++ lib/page/home.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key,required title});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
